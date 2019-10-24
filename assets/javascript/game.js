325596fd644100400d0895463d6cfe7132a2ec3a
Miguel Delos Santos <mendoza.d.santos@gmail.com>
2019-10-24 08:51:35 -0700

    finished basic html layout

---
+++ assets/javascript/game.js
@@ -0,0 +1,15 @@
+// ready to call for jquery
+
+// $(document).ready(function () {
+
+    // need viariable for words 
+    // variable for letters guessed 
+    // var for number of guesses 
+    // var for number of guesses left 
+
+    // function for when user presses a key 
+    // function for if the letter is in the word 
+    // function that resets the game and choose another word after 
+
+
+// }
