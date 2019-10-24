480bc26734c068c6ab6b3f37da8958972266fd13
Miguel Delos Santos <mendoza.d.santos@gmail.com>
2019-10-24 10:13:46 -0700

    outline of what javascript functions I need, and variables, in game.js

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,6 +1,19 @@
-// ready to call for jquery
 
-// $(document).ready(function () {
+
+var possiblewords = ["tacos", "burritos", "enchiladas", "chimichanga", "churro", "quesadilla", "tacquito",]
+// need function that increases wins when a won is achieved 
+var wins = 0
+// need function that shows guesses left 
+var guessesleft
+// need function that shows letters guessed 
+var lettersguessed = []
+// need functino that chooses from possiblewords at random 
+var mainword
+
+// need function for onkeypress at beginning
+
+
+
 
     // need viariable for words 
     // variable for letters guessed 
@@ -11,5 +24,3 @@
     // function for if the letter is in the word 
     // function that resets the game and choose another word after 
 
-
-// }
