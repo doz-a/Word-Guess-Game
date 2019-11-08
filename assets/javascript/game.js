4d46d9293a90cd4eddeff5cc31c3fe4b7503319c
Miguel Delos Santos <mendoza.d.santos@gmail.com>
2019-11-07 22:19:06 -0800

    added function that does not allow repeat letters, and a function that only allows letters (and capital letters) to be used

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -32,37 +32,52 @@ document.onkeyup = function (event) {
     var playerguess = event.key;
     console.log(playerguess);
 
-    // if player guesses right, wins score+1, the game is reset, updates instructions
-    if (playerguess === mainletter) {
-        wins++;
-        guessesleft = 12;
-        lettersguessed = [];
-        document.getElementById('instructions').innerHTML = "Wowz you guessed it!";
-        generator()
-    }
-    // if player guesses wrong they lose a guess, add a letter to lettersguessed array, updates instructions
-    if (playerguess !== mainletter) {
-        guessesleft--;
-        lettersguessed.push(playerguess);
-        document.getElementById('lettersguessed').innerHTML = lettersguessed;
-        document.getElementById('instructions').innerHTML = "Keep smashing the keyboard!";
-        console.log(lettersguessed);
-    }
-    // if player loses by guesses left lowering to 0, game is reset, losses +1, documents the computer-chosen letter in the mainletterx div, updates instructions, resets game with my generator function
-    if (guessesleft == 0) {
-        losses++;
-        lettersguessed = [];
-        guessesleft = 12;
-        document.getElementById('mainletterx').innerHTML = mainletter;
-        document.getElementById('instructions').innerHTML = "You destroyed the keyboard. Smash another one!";
-        generator()
-    }
+    // only use letters in the game 
+    var x = parseInt(event.keyCode)
+    if ((x >= 65 && x <= 90) || (x >= 97 && x <= 122)) {
+        // if player guesses right, wins score+1, the game is reset, updates instructions
+        if (playerguess === mainletter) {
+            wins++;
+            guessesleft = 12;
+            lettersguessed = [];
+            document.getElementById('instructions').innerHTML = "Wowz you guessed it!";
+            generator()
+        }
+
+        // if the player guessed the letter already 
+        else if (lettersguessed.indexOf(playerguess) > -1) {
+            document.getElementById('instructions').innerHTML = "You already used this letter!";
+            // guessesleft++;
+        }
+
+        // if player guesses wrong they lose a guess, add a letter to lettersguessed array, updates instructions
+        else if (playerguess !== mainletter) {
+            guessesleft--;
+            lettersguessed.push(playerguess);
+            document.getElementById('lettersguessed').innerHTML = lettersguessed;
+            document.getElementById('instructions').innerHTML = "Keep smashing the keyboard!";
+            console.log(lettersguessed);
+        }
+
+        // if player loses by guesses left lowering to 0, game is reset, losses +1, documents the computer-chosen letter in the mainletterx div, updates instructions, resets game with my generator function
+        if (guessesleft == 0) {
+            losses++;
+            lettersguessed = [];
+            guessesleft = 12;
+            document.getElementById('mainletterx').innerHTML = mainletter;
+            document.getElementById('instructions').innerHTML = "You destroyed the keyboard. Smash another one!";
+            generator()
+        }
 
 
-    // updates the wins, losses, guessesleft divs  
-    document.getElementById("wins").innerHTML = wins;
-    document.getElementById("losses").innerHTML = losses;
-    document.getElementById("guessesleft").innerHTML = guessesleft;
+        // updates the wins, losses, guessesleft divs  
+        document.getElementById("wins").innerHTML = wins;
+        document.getElementById("losses").innerHTML = losses;
+        document.getElementById("guessesleft").innerHTML = guessesleft;
+    }
+    else {
+        alert("invalid letter")
+    }
 
 }
 
